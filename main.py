b7ef04782b38cb2055db1a3c1f40aec63e546810
KuoHu <y066ydg5@duck.com>
2024-09-16 19:28:45 +0800

    feat(WebSocket): 新增对 ping 包的处理

--- main.py
+++ main.py
@@ -75,6 +75,10 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
+    @staticmethod
+    async def send_personal_message(message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
@@ -186,6 +190,9 @@ async def websocket_endpoint(websocket: WebSocket):
         while True:
             data = await websocket.receive_text()
             logger.info(f"Received data: {data}")
+            if data == "ping":
+                await manager.send_personal_message("pong", websocket)
+                logger.info(f"收到来自 {websocket.client.host} 的 ping 包，回复 pong")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
