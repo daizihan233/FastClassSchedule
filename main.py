ddf91c004cf36077ee69d6d5b908cb7a80155a35
KuoHu <y066ydg5@duck.com>
2024-10-19 20:46:59 +0800

    style(ConnectionManager): 移动至 utils/ws.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from loguru import logger
 
 from utils import config as utils_config
 from utils import weather
+from utils.ws import ConnectionManager
 
 app = FastAPI()
 security = HTTPBasic()
@@ -63,25 +64,6 @@ logger.add(
 
 if not pathlib.Path("./data/").exists():
     pathlib.Path("./data/").mkdir()
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    @staticmethod
-    async def send_personal_message(message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
 
 websocket_clients: dict[tuple[str, int], ConnectionManager] = {}
 
