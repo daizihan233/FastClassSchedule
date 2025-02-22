2916c0d400b9d370bd70e1760bbbf3a404a9c6a1
KuoHu <y066ydg5@duck.com>
2025-02-22 21:06:10 +0800

    ✨ feat: /api/update 增加报错提示

--- main.py
+++ main.py
@@ -322,6 +322,8 @@ async def api_update():
     match config.ci.kind:
         case "jenkins":
             return await ci.get_from_jenkins(config.ci.url, config.ci.filename)
+        case _:
+            raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="未适配此 CI 类型")
 
 
 
