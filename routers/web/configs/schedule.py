21240e89e0fbb0fe61ca6c71501fe7e42b40c29a
daizihan233 <y066ydg5@duck.com>
2025-08-30 03:20:05 +0800

    ✨ feat(schedule): 校验 daily_class.timetable 是否在 timetable 中，缺失时指定为“常日”

--- routers/web/configs/schedule.py
+++ routers/web/configs/schedule.py
@@ -20,6 +20,9 @@ def get_schedule(school: str, grade: str, cls: str):
     )["timetable"]
     max_subjects = max([max([v for v in timetable[x].values() if isinstance(v, int)]) for x in timetable.keys()]) + 1
     for index_d, day in enumerate(schedule["daily_class"]):
+        # 校验 daily_class.timetable 是否在 timetable 中，不存在则指定为“常日”
+        if "timetable" in day and day["timetable"] not in timetable:
+            schedule["daily_class"][index_d]["timetable"] = "常日"
         for index_i, item in enumerate(day["classList"]):
             if isinstance(item, str):
                 schedule["daily_class"][index_d]["classList"][index_i] = [
@@ -68,4 +71,3 @@ async def update_schedule(
     except KeyError:
         logger.warning(f"没有找到对应的websocket连接：{school} {grade}")
     return ORJSONResponse({'status': 200})
-
