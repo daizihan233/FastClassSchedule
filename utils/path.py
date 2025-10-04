457ed62375dbfba140c615a76850917bada1d3e8
daizihan233 <y066ydg5@duck.com>
2025-10-04 21:40:36 +0800

    Revert "feat(autorun): 支持时间表规则"
    This reverts commit 905ca4234830ba5d682bd80ab328bc29b47884cf.

--- utils/path.py
+++ utils/path.py
@@ -1,18 +1,5 @@
-import asyncio
 import pathlib
 
-
-def _discovery_path(path, directory=True, file=False, depth=1) -> list[str]:
-    root = pathlib.Path(path)
-    lst = root.glob('/'.join(["*"] * depth))
-    if directory and file:
-        return [str(p.relative_to(root)) for p in lst]
-    elif directory:
-        return [str(p.relative_to(root)) for p in lst if p.is_dir()]
-    elif file:
-        return [str(p.relative_to(root)) for p in lst if p.is_file()]
-    else:
-        return []  # But why?
 
 async def discovery_path(path, directory=True, file=False, depth=1) -> list[str]:
     """
@@ -23,4 +10,13 @@ async def discovery_path(path, directory=True, file=False, depth=1) -> list[str]
     :param depth: 深度
     :return: 一个列表，包含目录中的所有文件或文件夹的相对路径（不以 ./ 开头）
     """
-    return await asyncio.to_thread(_discovery_path, path, directory=directory, file=file, depth=depth)
+    root = pathlib.Path(path)
+    lst = root.glob('/'.join(["*"] * depth))
+    if directory and file:
+        return [str(p.relative_to(root)) for p in lst]
+    elif directory:
+        return [str(p.relative_to(root)) for p in lst if p.is_dir()]
+    elif file:
+        return [str(p.relative_to(root)) for p in lst if p.is_file()]
+    else:
+        return []  # But why?
