5ab39051161f2252d644fd25b3aafeef94e8dca9
daizihan233 <y066ydg5@duck.com>
2025-12-14 02:32:13 +0800

    feat(weather): 增强天气API，增加基于地理位置查询功能并改进错误处理机制。

--- routers/client/weather.py
+++ routers/client/weather.py
@@ -8,6 +8,7 @@ from utils.config import config
 
 router = APIRouter()
 
+
 @router.get("/api/weather/{province}/{name}", response_class=ORJSONResponse)
 @router.get("/api/weather/{name}", response_class=ORJSONResponse)
 async def weather_province_name(name: str, province: str = None):
@@ -19,32 +20,78 @@ async def weather_province_name(name: str, province: str = None):
     """
     for _ in range(5):
         try:
+            location = await weather.city_lookup(
+                name=name,
+                adm=province,
+                host=config.apikey.apihost,
+                key=config.apikey.weather,
+            )["name"]
             resp = await weather.weather_lookup_by_name(
                 name=name,
                 adm=province,
                 host=config.apikey.apihost,
-                key=config.apikey.weather
+                key=config.apikey.weather,
             )
             warn_resp = await weather.weather_warning_lookup_by_name(
                 name=name,
                 adm=province,
                 host=config.apikey.apihost,
-                key=config.apikey.weather
+                key=config.apikey.weather,
             )
-            warn = '；'.join([x['text'] for x in warn_resp['warning']]).replace('\n', '')
-            brief_warn = '；'.join([x['title'] for x in warn_resp['warning']]).replace('\n', '')
-            temp = resp['now']['temp']
-            weat = resp['now']['text']
-            logger.info(f"获取 {province}/{name} 的天气信息，T: {temp}, W: {weat}, Warning: {warn}, Brief: {brief_warn}")
-            return ORJSONResponse({"temp": temp, "weat": weat, 'warn': warn, 'brief_warn': brief_warn})
+            warn = "；".join([x["description"] for x in warn_resp["alerts"]]).replace(
+                "\n", ""
+            )
+            brief_warn = "；".join(
+                [x["headline"] for x in warn_resp["alerts"]]
+            ).replace("\n", "")
+            temp = resp["now"]["temp"]
+            weat = resp["now"]["text"]
+            wind = resp["now"]["windDir"]
+            wind_power = resp["now"]["windScale"]
+            logger.info(
+                f"获取 {province}/{name}（{location}） 的天气信息，T: {temp}, W: {weat}, Warning: {warn}, "
+                f"Brief: {brief_warn}, Wind: {wind} ({wind_power}级)"
+            )
+            return ORJSONResponse(
+                {
+                    "where": location,
+                    "temp": temp,
+                    "weat": weat,
+                    "wind": wind,
+                    "wind_power": wind_power,
+                    "warn": warn,
+                    "brief_warn": brief_warn,
+                }
+            )
         except KeyError:
             logger.error(f"不存在 {province}/{name} ")
-            return ORJSONResponse({"temp": 404, "weat": "不存在", "warning": '', 'brief_warn': ''}, status_code=status.HTTP_404_NOT_FOUND)
+            return ORJSONResponse(
+                {"temp": 404, "weat": "不存在", "warning": "", "brief_warn": ""},
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
         except Exception as err:
             logger.exception(f"获取天气信息失败: {err}", exc_info=True)
-    statistic['weather_error'] += 1
+    statistic["weather_error"] += 1
     logger.error(f"获取 {province}/{name} 的天气信息失败，超过最大重试次数")
     raise HTTPException(
         status_code=status.HTTP_502_BAD_GATEWAY,
-        detail="获取天气信息失败，超过最大重试次数，可能是上游服务器异常，或是本服务器存在网络波动"
+        detail="获取天气信息失败，超过最大重试次数，可能是上游服务器异常，或是本服务器存在网络波动",
     )
+
+
+@router.get("/api/weather/", response_class=ORJSONResponse)
+async def weather_by_cf(request):
+    """
+    通过请求来源获取对应城市的天气信息（通过 Cloudflare 请求头获取位置信息）
+    :return: 温度与天气
+    """
+    # 从 Cloudflare 请求头获取位置信息
+    city = request.headers.get("cf-ipcity")
+    region = request.headers.get("cf-region")
+    if not city:
+        logger.error("无法通过请求头获取城市信息")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="无法通过请求头获取城市信息，请确保请求经过 Cloudflare",
+        )
+    return await weather_province_name(name=city, province=region or None)
--- utils/weather.py
+++ utils/weather.py
@@ -3,7 +3,8 @@ from loguru import logger
 
 cache = {}
 
-async def city_lookup(name, key, host, adm=None):
+
+async def city_lookup(name, key, host, adm=None) -> dict:
     """
     查找某个城市的城市 ID
     :param host: API Host
@@ -13,17 +14,23 @@ async def city_lookup(name, key, host, adm=None):
     :return: 城市 ID
     """
     if (name, adm) in cache:
-        logger.info(f"Cache hit: {name = }, {adm = } -> {cache[(name, adm)]}")
+        logger.info(
+            f"Cache hit: {name = }, {adm = } -> {cache[(name, adm)]} | lat: {cache[(name, adm)]['lat']}, "
+            f"lon: {cache[(name, adm)]['lon']}"
+        )
         return cache[(name, adm)]
     async with aiohttp.ClientSession() as session:
         async with session.get(
-                f"https://{host}/geo/v2/city/lookup?"
-                f"location={name}&" + (f"&adm={adm}" if adm else ""),
-                headers={
-                    'X-QW-Api-Key': key
-                }
+            f"https://{host}/geo/v2/city/lookup?"
+            f"location={name}&" + (f"&adm={adm}" if adm else ""),
+            headers={"X-QW-Api-Key": key},
         ) as response:
-            cache[(name, adm)] = (await response.json())["location"][0]["id"]
+            cache[(name, adm)] = {
+                "id": (await response.json())["location"][0]["id"],
+                "lat": (await response.json())["location"][0]["lat"],
+                "lon": (await response.json())["location"][0]["lon"],
+                "name": (await response.json())["location"][0]["name"],
+            }
             return cache[(name, adm)]
 
 
@@ -35,34 +42,30 @@ async def weather_lookup(location, key, host):
     :param key: APIKEY
     :return: API 所返回的 JSON 数据
     """
-    async with (aiohttp.ClientSession() as session):
+    async with aiohttp.ClientSession() as session:
         async with session.get(
-            f"https://{host}/v7/weather/now?"
-            f"location={location}",
-            headers={
-                'X-QW-Api-Key': key
-            }
+            f"https://{host}/v7/weather/now?location={location}",
+            headers={"X-QW-Api-Key": key},
         ) as response:
             return await response.json()
 
 
-async def weather_warning_lookup(location, key, host):
+async def weather_warning_lookup(lat, lon, key, host):
     """
     查找某个城市的天气预警
+    :param lat: 所需位置的纬度
+    :param lon: 所需位置的经度
     :param host: API Host
-    :param location: 城市 ID
     :param key: APIKEY
     :return: API 所返回的 JSON 数据
     """
-    async with (aiohttp.ClientSession() as session):
+    async with aiohttp.ClientSession() as session:
         async with session.get(
-            f"https://{host}/v7/warning/now?"
-            f"location={location}",
-            headers={
-                'X-QW-Api-Key': key
-            }
+            f"https://{host}/weatheralert/v1/current/{lat}/{lon}",
+            headers={"X-QW-Api-Key": key},
         ) as response:
             return await response.json()
+
 
 async def weather_lookup_by_name(name, key, host, adm=None):
     """
@@ -74,9 +77,7 @@ async def weather_lookup_by_name(name, key, host, adm=None):
     :return: API 所返回的 JSON 数据
     """
     return await weather_lookup(
-        location=await city_lookup(name, key, host, adm),
-        host=host,
-        key=key
+        location=(await city_lookup(name, key, host, adm))["id"], host=host, key=key
     )
 
 
@@ -89,8 +90,5 @@ async def weather_warning_lookup_by_name(name, key, host, adm=None):
     :param adm: 省份名称
     :return: API 所返回的 JSON 数据
     """
-    return await weather_warning_lookup(
-        location=await city_lookup(name, key, host, adm),
-        host=host,
-        key=key
-    )
+    _, lat, lon, _ = (await city_lookup(name, key, host, adm)).values()
+    return await weather_warning_lookup(lat=lat, lon=lon, host=host, key=key)
